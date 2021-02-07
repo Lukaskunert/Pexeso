2c25104cfdde4a44290237af9fc455f0806b5b2a
Lukaskunert <36969999+Lukaskunert@users.noreply.github.com>
2021-02-07 17:23:55 +0100

    asdasdasdas
    dsadasdasdasdsadasdasdas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,40 @@ export class AppComponent {
   title = 'Pexesou';
 
   savedCard: Karticka | null = null;
+  number: number;
+  BREH: boolean;
+  LUL: boolean;
 
   karticky: Karticka[] = [
     {ikona: "1", otoceno: false},
-    {ikona: "2", otoceno: false},
-    {ikona: "3", otoceno: false},
-    {ikona: "4", otoceno: false},
-    {ikona: "5", otoceno: false},
-    {ikona: "6", otoceno: false},
     {ikona: "1", otoceno: false},
     {ikona: "2", otoceno: false},
+    {ikona: "2", otoceno: false},
+    {ikona: "3", otoceno: false},
     {ikona: "3", otoceno: false},
     {ikona: "4", otoceno: false},
+    {ikona: "4", otoceno: false},
     {ikona: "5", otoceno: false},
+    {ikona: "5", otoceno: false},
+    {ikona: "6", otoceno: false},
     {ikona: "6", otoceno: false}
   ];
+
+  clickedButton() {
+    this.BREH = true;
+    this.LUL = true;
+    this.karticky.length = this.number * 2;
+  }
+  if (this.number < 6) {
+    var BREH = this.karticky.length,temporaryValue,randomIndex;
+  } else if (this.number = 0) {
+    this.number = 0;
+  } else {
+    System.out.println("nešlo nic");
+    this.number = null;
+  }
+
+  // netušim co dal
 
   otocit(clicknutaKarticka: Karticka): void {
     if (clicknutaKarticka.otoceno === true) {
